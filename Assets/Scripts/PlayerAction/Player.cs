65f5cd57e061b0893796638ec34cd07dd35de678
Shinpo <21ci0222@jec.ac.jp>
2022-12-01 12:21:29 +0900

    プレイヤの移動をRigidbody2Dに変更

--- Assets/Scripts/PlayerAction/Player.cs
+++ Assets/Scripts/PlayerAction/Player.cs
@@ -6,9 +6,12 @@ public class Player : StateMachineBase<Player>
 {
     public float speed = 4.0f;
 
+    private Rigidbody2D objRigidbody;
+
     private void Start()
     {
         ChangeState(new Player.Idle(this));
+
     }
 
     private class Idle : StateBase<Player>
@@ -20,6 +23,7 @@ public class Player : StateMachineBase<Player>
         //開始時1回
         public override void OnEnterState()
         {
+            machine.objRigidbody = machine.GetComponent<Rigidbody2D>();
             Debug.Log("Idle開始");
         }
 
@@ -64,50 +68,46 @@ public class Player : StateMachineBase<Player>
             {
                 if (Input.GetKey(KeyCode.A))
                 {
-                    machine.transform.Translate(Vector3.Normalize(Vector3.up + Vector3.left) * machine.speed * Time.deltaTime);
+                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.up + Vector3.left) * machine.speed * Time.deltaTime);
                     return;
                 }
                 if (Input.GetKey(KeyCode.D))
                 {
-                    machine.transform.Translate(Vector3.Normalize(Vector3.up + Vector3.right) * machine.speed * Time.deltaTime);
+                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.up + Vector3.right) * machine.speed * Time.deltaTime);
                     return;
                 }
                 Debug.Log("↑");
-                machine.transform.Translate(Vector3.up * machine.speed * Time.deltaTime);
+                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.up * machine.speed * Time.deltaTime);
             }
             if (Input.GetKey(KeyCode.S))
             {
                 if (Input.GetKey(KeyCode.A))
                 {
-                    machine.transform.Translate(Vector3.Normalize(Vector3.down + Vector3.left) * machine.speed * Time.deltaTime);
+                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.down + Vector3.left) * machine.speed * Time.deltaTime);
                     return;
                 }
                 if (Input.GetKey(KeyCode.D))
                 {
-                    machine.transform.Translate(Vector3.Normalize(Vector3.down + Vector3.right) * machine.speed * Time.deltaTime);
+                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.down + Vector3.right) * machine.speed * Time.deltaTime);
                     return;
                 }
 
                 Debug.Log("↓");
-                machine.transform.Translate(Vector3.down * machine.speed * Time.deltaTime);
+                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.down * machine.speed * Time.deltaTime);
             }
             if (Input.GetKey(KeyCode.A))
             {
                 Debug.Log("←");
-                machine.transform.Translate(Vector3.left * machine.speed * Time.deltaTime);
+                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.left * machine.speed * Time.deltaTime);
             }
             if (Input.GetKey(KeyCode.D))
             {
                 Debug.Log("→");
-                machine.transform.Translate(Vector3.right * machine.speed * Time.deltaTime);
+                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.right * machine.speed * Time.deltaTime);
             }
             
         }
 
-        public override void OnExitState()
-        {
-            machine.ChangeState(new Player.Idle(machine));
-        }
     }
 
     private class Attack : StateBase<Player>
