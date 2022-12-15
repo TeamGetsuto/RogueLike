3a669017c5ec8969cf5799dec154cb4bf7729763
Shinpo <21ci0222@jec.ac.jp>
2022-12-15 12:24:13 +0900

    攻撃と防御の状態を追加した（他も調整）

--- Assets/Scripts/PlayerAction/Player.cs
+++ Assets/Scripts/PlayerAction/Player.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : StateMachineBase<Player>
 {
     public float speed = 4.0f;
+    private bool isChecked_Input = false;
+    private bool isChecked_MoveKey = false;
+    KeyCode key;
 
     private Rigidbody2D objRigidbody;
+
 
     private void Start()
     {
         ChangeState(new Player.Idle(this));
-
     }
 
+    //何もしていない
     private class Idle : StateBase<Player>
     {
         public Idle(Player _machine) : base(_machine)
@@ -23,8 +28,9 @@ public class Player : StateMachineBase<Player>
         //開始時1回
         public override void OnEnterState()
         {
-            machine.objRigidbody = machine.GetComponent<Rigidbody2D>();
+            player.objRigidbody = player.GetComponent<Rigidbody2D>();
             Debug.Log("Idle開始");
+            
         }
 
         //毎フレーム呼ばれる
@@ -32,23 +38,32 @@ public class Player : StateMachineBase<Player>
         {
             if (Input.GetKey(KeyCode.W))
             {
-                machine.ChangeState(new Player.Walk(machine));
+                player.ChangeState(new Player.Walk(player));
             }
             if (Input.GetKey(KeyCode.S))
             {
-                machine.ChangeState(new Player.Walk(machine));
+                player.ChangeState(new Player.Walk(player));
             }
             if (Input.GetKey(KeyCode.A))
             {
-                machine.ChangeState(new Player.Walk(machine));
+                player.ChangeState(new Player.Walk(player));
             }
             if (Input.GetKey(KeyCode.D))
             {
-                machine.ChangeState(new Player.Walk(machine));
+                player.ChangeState(new Player.Walk(player));
+            }
+            if (Input.GetKey(KeyCode.F))
+            {
+                player.ChangeState(new Player.Attack(player));
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                player.ChangeState(new Player.Defense(player));
             }
         }
     }
 
+    //歩行
     private class Walk : StateBase<Player>
     {
         public Walk(Player _machine) : base(_machine)
@@ -58,62 +73,212 @@ public class Player : StateMachineBase<Player>
         //開始時1回
         public override void OnEnterState()
         {
+            player.isChecked_Input = true;
             Debug.Log("歩行開始");
         }
 
         //毎フレーム呼ばれる
         public override void OnUpdate()
         {
-            if (Input.GetKey(KeyCode.W))
+            if (player.isChecked_Input)
             {
-                if (Input.GetKey(KeyCode.A))
+                if (Input.GetKey(KeyCode.W))
                 {
-                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.up + Vector3.left) * machine.speed * Time.deltaTime);
-                    return;
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        player.ChangeState(new Player.Defense(player));
+                    }
+                    if (Input.GetKey(KeyCode.F))
+                    {
+                        player.ChangeState(new Player.Attack(player));
+                    }
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        Debug.Log("左上");
+                        player.MoveDirection(player, Vector3.Normalize(Vector3.up + Vector3.left));
+                        return;
+                    }
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        Debug.Log("右上");
+                        player.MoveDirection(player, Vector3.Normalize(Vector3.up + Vector3.right));
+                        return;
+                    }
+
+                    //Debug.Log("上");
+                    player.MoveDirection(player, Vector3.up);
                 }
-                if (Input.GetKey(KeyCode.D))
+                else if (Input.GetKeyUp(KeyCode.W))
                 {
-                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.up + Vector3.right) * machine.speed * Time.deltaTime);
-                    return;
-                }
-                Debug.Log("↑");
-                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.up * machine.speed * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                if (Input.GetKey(KeyCode.A))
-                {
-                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.down + Vector3.left) * machine.speed * Time.deltaTime);
-                    return;
-                }
-                if (Input.GetKey(KeyCode.D))
-                {
-                    machine.objRigidbody.MovePosition(machine.transform.position + Vector3.Normalize(Vector3.down + Vector3.right) * machine.speed * Time.deltaTime);
-                    return;
+                    player.isChecked_Input = false;
                 }
 
-                Debug.Log("↓");
-                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.down * machine.speed * Time.deltaTime);
+                if (Input.GetKey(KeyCode.S))
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        player.ChangeState(new Player.Defense(player));
+                    }
+                    if (Input.GetKey(KeyCode.F))
+                    {
+                        player.ChangeState(new Player.Attack(player));
+                    }
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        Debug.Log("左下");
+                        player.MoveDirection(player, Vector3.Normalize(Vector3.down + Vector3.left));
+                        return;
+                    }
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        Debug.Log("右下");
+                        player.MoveDirection(player, Vector3.Normalize(Vector3.down + Vector3.right));
+                        return;
+                    }
+
+                    Debug.Log("下");
+                    player.MoveDirection(player, Vector3.down);
+                }
+                else if (Input.GetKeyUp(KeyCode.S))
+                {
+                    player.isChecked_Input = false;
+                }
+
+
+                if (Input.GetKey(KeyCode.A))
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        player.ChangeState(new Player.Defense(player));
+                    }
+                    if (Input.GetKey(KeyCode.F))
+                    {
+                        player.ChangeState(new Player.Attack(player));
+                    }
+                    Debug.Log("左");
+                    player.MoveDirection(player, Vector3.left);
+                }
+                else if (Input.GetKeyUp(KeyCode.A))
+                {
+                    player.isChecked_Input = false;
+                }
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        player.ChangeState(new Player.Defense(player));
+                    }
+                    if (Input.GetKey(KeyCode.F))
+                    {
+                        player.ChangeState(new Player.Attack(player));
+                    }
+                    Debug.Log("右");
+                    player.MoveDirection(player, Vector3.right);
+                }
+                else if (Input.GetKeyUp(KeyCode.D))
+                {
+                    player.isChecked_Input = false;
+                }
             }
-            if (Input.GetKey(KeyCode.A))
+            else
             {
-                Debug.Log("←");
-                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.left * machine.speed * Time.deltaTime);
+                player.ChangeState(new Player.Idle(player));
             }
-            if (Input.GetKey(KeyCode.D))
-            {
-                Debug.Log("→");
-                machine.objRigidbody.MovePosition(machine.transform.position + Vector3.right * machine.speed * Time.deltaTime);
-            }
-            
+
         }
 
     }
 
+    //攻撃
     private class Attack : StateBase<Player>
     {
         public Attack(Player _machine) : base(_machine)
         {
         }
+
+        public override void OnEnterState()
+        {
+            player.isChecked_Input = true;
+            Debug.Log("攻撃開始");
+        }
+
+        public override void OnUpdate()
+        {
+            if (player.isChecked_Input)
+            {
+                if (Input.GetKeyUp(KeyCode.F))
+                {
+                    if (Input.anyKey)
+                    {
+                        player.isChecked_MoveKey = true;
+                    }
+                    player.isChecked_Input = false;
+                }
+            }
+            else if (!player.isChecked_Input && player.isChecked_MoveKey)
+            {
+                player.isChecked_MoveKey = false;
+                player.ChangeState(new Player.Walk(player));
+            }
+            else if (!player.isChecked_Input && !player.isChecked_MoveKey)
+            {
+                player.ChangeState(new Player.Idle(player));
+            }
+        }
     }
+
+    //防御
+    private class Defense : StateBase<Player>
+    {
+        public Defense(Player _machine) : base(_machine)
+        {
+        }
+
+        public override void OnEnterState()
+        {
+            player.isChecked_Input = true;
+            Debug.Log("防御開始");
+        }
+
+        public override void OnUpdate()
+        {
+            if (player.isChecked_Input)
+            {
+                if (Input.GetKeyUp(KeyCode.LeftShift))
+                {
+                    if (Input.anyKey)
+                    {
+                        player.isChecked_MoveKey = true;
+                    }
+                    player.isChecked_Input = false;
+                }
+            }
+            else if (!player.isChecked_Input && player.isChecked_MoveKey)
+            {
+                player.isChecked_MoveKey = false;
+                player.ChangeState(new Player.Walk(player));
+            }
+            else if (!player.isChecked_Input && !player.isChecked_MoveKey)
+            {
+                player.ChangeState(new Player.Idle(player));
+            }
+        }
+    }
+
+    //移動
+    public void MoveDirection(Player player, Vector3 vector)
+    {
+        player.objRigidbody.MovePosition(player.transform.position + vector * player.speed * Time.deltaTime);
+    }
+
+    //public void InputKey(Player player, KeyCode key)
+    //{
+    //    switch(key)
+    //    {
+    //        case KeyCode.W:
+    //            MoveDirection(player, Vector3.up);
+    //            break;
+    //    }
+    //}
 }
--- Assets/Scripts/PlayerAction/StateBase.cs
+++ Assets/Scripts/PlayerAction/StateBase.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class StateBase<T> where T :StateMachineBase<T>
 {
-	protected T machine;
-	public StateBase(T _machine) { machine = _machine; }
+	protected T player;
+	private bool test;
+	public StateBase(T _machine) { player = _machine; }
 	//開始時に1回　初期化
 	public virtual void OnEnterState() { }
 	//毎フレーム呼ばれる　通常のアップデートと同じタイミングで
